255171df666f2be19610683b87444b29fc79d27d
Krishna <joi@aigenlayer.org>
2025-11-12 21:23:44 +0545

    solved https

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'http://16.171.111.4:3001/:path*',
+      },
+    ];
+  },
 }
 
 module.exports = nextConfig
